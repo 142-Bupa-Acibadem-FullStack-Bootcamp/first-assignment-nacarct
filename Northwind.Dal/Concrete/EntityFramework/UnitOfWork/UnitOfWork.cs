e58ba5c4e645a78031f307b3f91c1aef0187f25b
nacar <nacar@HUAWEI>
2021-12-24 21:26:56 +0300

    Ödev Yapıldı :)

---
+++ Northwind.Dal/Concrete/EntityFramework/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using Northwind.Dal.Abstract;
+
+namespace Northwind.Dal.Concrete.EntityFramework.UnitOfWork
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool BeginTransaction()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RollBackTransaction()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int SaveChange()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Northwind.Interface/IGenericService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Northwind.Entity.IBase;
+
+namespace Northwind.Interface
+{
+    public interface IGenericService<T, TDto> where T:IEntityBase where TDto:IDtoBase
+    {
+        List<TDto> GetAll();
+        List<TDto> GetAll(Expression<Func<T, bool>> expression);
+        TDto Find(int id);
+        IQueryable<T> GetQueryable();
+        TDto Add(TDto entity);
+        Task<TDto> AddAsync(TDto entity);
+        TDto Update(TDto entity);
+        Task<TDto> UpdateAsync(TDto entity);
+        bool DeleteById(int id);
+        Task<bool> DeleteByIdAsync(int id);
+        bool Delete(TDto entity);
+        Task<bool> DeleteAsync(TDto entity);
+    }
+}
